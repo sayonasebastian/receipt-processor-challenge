2c462b2799aa69458bf26277684e0b96fb18815b
sayonasebastian <53229496+sayonasebastian@users.noreply.github.com>
2024-07-09 21:49:23 -0400

    Added dockerfile Updated index.js file to handle points calculation

--- index.js
+++ index.js
@@ -16,15 +16,17 @@ const calculatePoints = function (obj) {
   const regX = /[a-zA-Z0-9]/g;
   const matches = obj.retailer.match(regX);
   points += matches ? matches.length : 0;
-  points += obj.total === Math.round(obj.total) ? 50 : 0;
+  points += (obj.total.split(".")[1] === "00") ? 50 : 0;
   points += obj.total % 0.25 === 0 ? 25 : 0;
   points += Math.floor(obj.items.length / 2) * 5;
   points += obj.items.reduce((total, value) => {
     var l = value.shortDescription.trim().length;
     return total + Math.ceil(l % 3 === 0 ? value.price * 0.2 : 0);
   }, 0);
+  console.log(new Date(obj.purchaseDate));
   points += (new Date(obj.purchaseDate)).getDate() % 2 !== 0 ? 6 : 0;
-  points += parseInt(obj.purchaseTime.split(':')[0]) > 2 && parseInt(obj.purchaseTime.split(':')[0]) < 4 ? 10 : 0;
+  points += (new Date(`${obj.purchaseDate} ${obj.purchaseTime}`)) > (new Date(`${obj.purchaseDate} 14:00`))
+  && (new Date(`${obj.purchaseDate} ${obj.purchaseTime}`)) < (new Date(`${obj.purchaseDate} 16:00`)) ? 10 : 0;
   return points;
 }
 app.post('/receipts/process', (req, res) => {
